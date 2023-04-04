126d51e8e7f9a4537c3da7a0ac535baf2fdf45f0
FinnyLove <125204541+FinnyLove@users.noreply.github.com>
2023-04-04 17:15:40 +0700

    Create interchangesort_1.cpp

---
+++ interchangesort_1.cpp
@@ -0,0 +1,20 @@
+void swap(int &a, int &b){
+	int x = a;
+	a = b;
+	b = x;
+}
+
+void InterchangeSort(int a[], int n){
+	for(int i = 0; i < n-1; i++)
+	{
+		for(int j = i+1; j < n; j++)
+		{
+			if(a[i] > a[j])
+			{
+				swap(a[i], a[j]);
+			}	
+		}
+	}
+}
+
+int main()
